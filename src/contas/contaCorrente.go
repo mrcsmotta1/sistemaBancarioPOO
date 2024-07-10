32d4f678ea757f2637515605ece8b43444f2f851
Marcos Motta <mrcsmotta1@gmail.com>
2024-07-10 19:53:52 -0300

    Alteradno visibilidade de saldo e criando obter saldo publico

--- src/contas/contaCorrente.go
+++ src/contas/contaCorrente.go
@@ -6,14 +6,14 @@ type ContaCorrente struct {
 	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
 	if podeSacar {
-		c.Saldo -= valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso!"
 	}
 
@@ -24,22 +24,26 @@ func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	podeDepositar := valorDoDeposito > 0
 
 	if podeDepositar {
-		c.Saldo += valorDoDeposito
-		return "Deposito realizado com sucesso!", c.Saldo
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso!", c.saldo
 	}
 
 	return "Valor do Deposito menor que 0!", valorDoDeposito
 }
 
 func (c *ContaCorrente) Transferir(valorDeTransferencia float64, contDestino *ContaCorrente) bool {
-	podeTransferir := valorDeTransferencia > 0 && valorDeTransferencia <= c.Saldo
+	podeTransferir := valorDeTransferencia > 0 && valorDeTransferencia <= c.saldo
 
 	if podeTransferir {
-		c.Saldo -= valorDeTransferencia
+		c.saldo -= valorDeTransferencia
 		contDestino.Depositar(valorDeTransferencia)
 
 		return true
 	}
 
 	return false
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
