013eb8cffeb7915487b50ead89efffd70174c59c
Marcos Motta <mrcsmotta1@gmail.com>
2024-07-10 18:23:20 -0300

    Criando Sacar

--- src/main.go
+++ src/main.go
@@ -9,10 +9,23 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	}
+
+	return "Saldo Insuficiente!"
+}
+
 func main() {
 	contaDoMarcos := ContaCorrente{}
 	contaDoMarcos.titular = "Marcos"
 	contaDoMarcos.saldo = 500
 
+	fmt.Println(contaDoMarcos.saldo)
+	fmt.Println(contaDoMarcos.Sacar(200))
 	fmt.Println(contaDoMarcos.saldo)
 }
