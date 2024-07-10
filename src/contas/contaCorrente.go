2cbb80417aaee8360566af65886bc378c0fd9d7c
Marcos Motta <mrcsmotta1@gmail.com>
2024-07-10 19:18:23 -0300

    Criando pacote de Conta Corrente

---
+++ src/contas/contaCorrente.go
@@ -0,0 +1,43 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	}
+
+	return "Saldo Insuficiente!"
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	podeDepositar := valorDoDeposito > 0
+
+	if podeDepositar {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso!", c.Saldo
+	}
+
+	return "Valor do Deposito menor que 0!", valorDoDeposito
+}
+
+func (c *ContaCorrente) Transferir(valorDeTransferencia float64, contDestino *ContaCorrente) bool {
+	podeTransferir := valorDeTransferencia > 0 && valorDeTransferencia <= c.Saldo
+
+	if podeTransferir {
+		c.Saldo -= valorDeTransferencia
+		contDestino.Depositar(valorDeTransferencia)
+
+		return true
+	}
+
+	return false
+}
--- src/main.go
+++ src/main.go
@@ -1,59 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso!"
-	}
-
-	return "Saldo Insuficiente!"
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	// c.saldo += c.saldo
-	podeDepositar := valorDoDeposito > 0
-
-	if podeDepositar {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso!", c.saldo
-	}
-
-	return "Valor do Deposito menor que 0!", valorDoDeposito
-}
-
-func (c *ContaCorrente) Transferir(valorDeTransferencia float64, contDestino *ContaCorrente) bool {
-	podeTransferir := valorDeTransferencia > 0 && valorDeTransferencia <= c.saldo
-
-	if podeTransferir {
-		c.saldo -= valorDeTransferencia
-		contDestino.Depositar(valorDeTransferencia)
-
-		return true
-	}
-
-	return false
-
-}
+	"github.com/mrcsmotta1/sistemaBancarioPOO/src/contas"
+)
 
 func main() {
-	contaDoMarcos := ContaCorrente{}
-	contaDoMarcos.titular = "Marcos"
-	contaDoMarcos.saldo = 500
+	contaDoMarcos := contas.ContaCorrente{}
+	contaDoMarcos.Titular = "Marcos"
+	contaDoMarcos.Saldo = 500
 
-	contaDoGuilherme := ContaCorrente{}
-	contaDoGuilherme.titular = "Guilherme"
-	contaDoGuilherme.saldo = 1000
+	contaDoGuilherme := contas.ContaCorrente{}
+	contaDoGuilherme.Titular = "Guilherme"
+	contaDoGuilherme.Saldo = 1000
 
 	fmt.Println(contaDoMarcos)
 	fmt.Println(contaDoGuilherme)
