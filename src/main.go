b1a4e5a68fb9a9762919ac32aa161c07d8bb4119
Marcos Motta <mrcsmotta1@gmail.com>
2024-07-24 16:42:17 -0300

    Criaçao de conta poupança

--- src/main.go
+++ src/main.go
@@ -3,24 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/mrcsmotta1/sistemaBancarioPOO/src/clientes"
 	"github.com/mrcsmotta1/sistemaBancarioPOO/src/contas"
 )
 
 func main() {
-	clienteMarcos := clientes.Titular{
-		Nome:      "Marcos",
-		CPF:       "308.916.774-61",
-		Profissao: "Desenvolvedor",
-	}
+	contaPoupandaMarcos := contas.ContaPoupanca{}
+	contaPoupandaMarcos.Depositar(100)
+	contaPoupandaMarcos.Sacar(50)
 
-	contaDoMarcos := contas.ContaCorrente{Titular: clienteMarcos,
-		NumeroAgencia: 123,
-		NumeroConta:   123456,
-	}
-
-	fmt.Println(contaDoMarcos)
-
-	contaDoMarcos.Depositar(100)
-	fmt.Println(contaDoMarcos.ObterSaldo())
+	fmt.Println(contaPoupandaMarcos.ObterSaldo())
 }
